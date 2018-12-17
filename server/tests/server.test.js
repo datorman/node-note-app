08be3f02eda6ce54cd2df2018688a60ef13f3572
Datorman <mikeshutov@gmail.com>
2018-12-17 11:46:55 +0000

    Committing get test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,17 @@ const request  = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+    text:'First test dodo'
+},{
+    text:'Second test todo'
+}];
+
 beforeEach((done)=>{
-    Todo.remove({}).then(()=> done());
+    Todo.remove({}).then(()=> {
+        return Todo.insertMany(todos);
+    }).then(()=> done());
+    
 });
 
 describe('POST - TODOS', () =>{
@@ -22,7 +31,7 @@ describe('POST - TODOS', () =>{
                 if(error){
                     return done(error);
                 }
-                Todo.find().then((todos)=>{
+                Todo.find({text}).then((todos)=>{
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -41,11 +50,23 @@ describe('POST - TODOS', () =>{
                     return done(error);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((error) => {
                     done(error)
                 })  
             });
     })
+});
+
+describe('GET todos', () =>{
+    it('Should get all todos', (done) =>{
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((response)=>{
+                expect(response.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
 });
