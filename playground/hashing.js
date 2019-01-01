251510ba35a6ecb9fae9d8ab762e255cda030c4d
Mike Shutov <mikeshutov@gmail.com>
2019-01-01 16:24:55 -0500

    Hash User password before save to db

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,20 +1,36 @@
 const {SHA256} = require('crypto-js');
-
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 5
-};
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    console.log(salt);
+    bcrypt.hash(password, salt, (err, hash) =>{
+        console.log(hash);
+    })
+});
+
+var hashedPassword = '$2a$10$zOnv6d2kBkuH2mHtd35Hh.5amPhIhPvZ6OBNH2Sid5RwNmpTvf11C';
+
+
+bcrypt.compare(password,hashedPassword, (err, res)=> {
+    console.log(res);
+});
+
+// var data = {
+//     id: 5
+// };
 
 
 
-//jwt.sign
-var token = jwt.sign(data,'123abc');
-console.log(token);
+// //jwt.sign
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
 
-//jwt.verify
-var decoded = jwt.verify(token,'123abc');
-console.log('decoded',decoded);
+// //jwt.verify
+// var decoded = jwt.verify(token,'123abc');
+// console.log('decoded',decoded);
 
 // var message = 'I am a user message';
 
