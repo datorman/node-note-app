7fd8a3aa015a89a249ed8b280eec4198974f5d38
Datorman <mikeshutov@gmail.com>
2019-01-15 10:49:48 +0000

    Add delete users.me

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,16 @@ UserSchema.methods.generateAuthToken = function () {
         return token;
     });
 };
+UserSchema.methods.removeToken = function (token){
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    });
+};
 //Modal Methods
 UserSchema.statics.findByToken = function(token) {
     var User = this;
